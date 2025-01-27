bcad056d9035691dbd12ff5b7a624471ae9b8884
Omar Abdullah Mahran <109427483+Omar-A-Mahran9@users.noreply.github.com>
2025-01-27 19:29:47 +0200

    Biuld Dashborad and main compnent

--- database/migrations/2024_11_02_144619_create_contact_us_table.php
+++ database/migrations/2024_11_02_144619_create_contact_us_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('parent_name');
             $table->string('child_name');
-            $table->integer('phone');
+            $table->string('phone');
             $table->string('message');
             $table->timestamps();
         });
